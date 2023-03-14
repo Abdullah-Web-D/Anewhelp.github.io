394dae568656da31a7a4140f816c7e1a43443d39
Abdullah Web Developer <124901972+Abdullah-Web-D@users.noreply.github.com>
2023-03-14 19:44:24 +0400

    JavaScript

---
+++ Sr.js
@@ -0,0 +1,9 @@
+var x = document.getElementsByClassName("h1");
+console.log(x[0]);
+for (var i = 0; i < x.length; i++) {
+    x[i].getElementsByClassName.textDecoration = "none";
+
+}
+
+var newhack00 = document.getElementById("species");
+newhack00.innerHTML = "A new hack"
